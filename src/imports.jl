68886a715053cbf577160b9cbca1469a4d66a133
mgemath <muratore.g.e@gmail.com>
2025-09-30 13:36:50 +0200

    Update Oscar and add Graphs.jl

--- src/imports.jl
+++ src/imports.jl
@@ -3,5 +3,8 @@ import Oscar.direct_sum, Oscar.line_bundle
 import Oscar.IntersectionTheory.tangent_bundle
 import Oscar.IntersectionTheory.cotangent_bundle
 import Oscar: projective_space
-import Oscar: add_edge!, isvalid, blow_up, point_class, integrate, is_effective, chern_number, rank, dual, projectivization, tangent_bundle, cotangent_bundle, chern_class
+import Oscar: isvalid, blow_up, point_class, integrate, is_effective, chern_number, rank, dual, projectivization, tangent_bundle, cotangent_bundle, chern_class
 import Oscar: schubert_class, schubert_classes
+
+# Graph-functions
+import Oscar: add_vertex!, add_edge!, isvalid, is_connected, is_simple, is_loopless, neighbors, degree, indegree, outdegree, has_edge, has_vertex, vertices, edges, nv, ne
