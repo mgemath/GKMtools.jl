1b8cf7fd45fa0e617c0efcded4356c469193d13d
dh604 <daniel.holmes@gmx.at>
2026-01-07 15:11:39 +0100

    VPs vs H benchmark

---
+++ src/GW/PosGenus/Euler_pos_gen_3.jl
@@ -0,0 +1,90 @@
+# This is a copy of Euler_pos_gen_2.jl using a list of vertex polynomials instead of Hodge numbers.
+# It is only a temporary file to test whether storing vertex polys improves performance.
+# As opposed to Euler_pos_gen_2.jl, this file does not support psi classes.
+
+# Positive genus version of /src/GW/Euler.jl.
+function Euler_inv_pos_gen(dg::GW_decorated_graph, t::Vector{T}, edge_weight_dict::Dict{Edge, T}, point_weight_dict::Vector{Union{Nothing, T}}, psi_exp::Vector{Int64}, VPs::Matrix{QQMPolyRingElem}) where T<:RingElem
+
+  res = one(t[1])
+
+  for v in 1:n_vertices(dg.g)
+
+    gv = dg.genus[v]
+    valv_plus_g = valency(v, dg) + gv
+    # valv_plus_g_minus_one = valency(v, dg) + dg.genus[v] - 1
+    e = euler_class(imageOf(v, dg), dg.gkm.equivariantCohomology, t, edge_weight_dict, point_weight_dict)
+
+    # res = res * (e^valv_plus_g_minus_one) # This probably throws an error if valv_plus_g_minus_one = -1. That's why I used the six lines below originally.
+
+    #println("e = $e, val = $valv")
+    if valv_plus_g >= 1
+      res = res * e^(valv_plus_g - 1)
+    else
+      res = res // e
+    end
+
+
+    imV = imageOf(v, dg)
+    u = vcat((edgeMult(Edge(v, n), dg) .// weight_class(Edge(imV, imageOf(n, dg)), dg.gkm, t, edge_weight_dict) for n in all_neighbors(dg.g, v))...,)
+    w = [1 // weight_class(Edge(imV, n), dg.gkm, t, edge_weight_dict) for n in all_neighbors(dg.gkm.g, imV)]
+    
+    # psi_at_v = [psi_exp[i] for i in 1:length(dg.marks) if dg.marks[i] == v]
+    # println("psi_at_v = $psi_at_v")
+    # Lambda insertions are not used so far. 
+    # To implement them, we need to distribute each lambda over the positive genus vertices of dg.
+    # lambda_at_v = zeros(Int64, gv)
+
+    nMarks = count(i -> i==v, dg.marks)
+    vpEval = evaluate_vertex_polynomial(u, w, nMarks, dg.genus[v], VPs)
+    # vpEval = evaluate_vertex_polynomial_with_psis(u, w, psi_at_v, gv, H)
+    # println("vpEval = $(numerator(vpEval)) // $(denominator(vpEval))")
+    res = res * vpEval
+
+    # if dg.genus[v] > 0
+    #   println("vpEval = $(factor(numerator(vpEval))) // $(factor(denominator(vpEval)))")
+    #   println("u = $u")
+    #   println("w = $w")
+    #   println("nMarks = $nMarks")
+    # end
+  end
+
+  # if n_edges(dg.g) == 1 && sum(dg.genus) == 1
+  #   res = res // 4
+  # end
+
+  return res
+end
+
+# below not updated, as will be obsolete after transition to GKM graph with flags.
+# This returns the extra factor for Euler_inv_pos_gen in the fiber direction.
+function _Euler_inv_pos_gen_VB(dg::GW_decorated_graph, V::GKM_vector_bundle, VPs::Matrix{QQMPolyRingElem})::AbstractAlgebra.Generic.FracFieldElem{QQMPolyRingElem}
+
+  R = V.gkm.equivariantCohomology.coeffRing
+
+  res = one(R)
+
+  for v in 1:n_vertices(dg.g)
+
+    valv_plus_g = valency(v, dg) + dg.genus[v]
+    e = euler_class(imageOf(v, dg), dg.gkm.equivariantCohomology)
+    e = e * _fiber_normal_weight(imageOf(v, dg), V)
+    #println("e = $e, val = $valv")
+    if valv_plus_g >= 1
+      res = res * e^(valv_plus_g - 1)
+    else
+      res = res // e
+    end
+
+    imV = imageOf(v, dg)
+    u = vcat((edgeMult(Edge(v, n), dg) .// weight_class(Edge(imV, imageOf(n, dg)), dg.gkm) for n in all_neighbors(dg.g, v))...,)
+    w_from_base = [1 // weight_class(Edge(imV, n), dg.gkm) for n in all_neighbors(dg.gkm.g, imV)]
+    w_from_fibre = [1 // _fiber_summand_weight(imV, i, V) for i in 1:rank(V)]
+    w = vcat(w_from_base, w_from_fibre)
+    
+    nMarks = count(i -> i==v, dg.marks)
+    vpEval = evaluate_vertex_polynomial(u, w, nMarks, dg.genus[v], VPs)
+    res = res * vpEval
+  end
+  
+  return res
+end
--- test/GW/VP_timing.jl
+++ test/GW/VP_timing.jl
@@ -1,15 +1,44 @@
-n_repeats = 1
+n_repeats = 5
 
 F = gkm_3d_twisted_flag();
 beta = curve_class(F, "3", "4");
 
 @time begin
     for _ in 1:n_repeats
-        for d in 1:3
-            for g in 1:2
+        for d in 1:4
+            for g in 1:3
                 gw = gromov_witten(F, d*beta, 0, class_one(); g=g, show_bar=false)
                 println("Genus $g, degree $d: $gw")
             end
         end
     end
 end
+
+# We run the above code 3-4 times for the above code without psi classes.
+# the goal is to see if it makes sense to store vertex polynomials or if H is enough.
+
+######
+
+# n_repeats = 5, d in 1:3, g in 1:3, twisted flag class_one().
+
+# with VPs:
+# 	59.962779 seconds (18.95 M allocations: 918.598 MiB, 0.72% gc time, 7.78% compilation time)
+# 	59.121890 seconds (28.04 M allocations: 1.221 GiB, 1.13% gc time, 6.90% compilation time)
+# 	60.185003 seconds (28.04 M allocations: 1.221 GiB, 1.13% gc time, 6.69% compilation time)
+
+
+# with H:
+# 	61.457161 seconds (30.84 M allocations: 1.357 GiB, 1.20% gc time, 9.58% compilation time: 18% of which was recompilation)
+# 	62.782081 seconds (28.04 M allocations: 1.222 GiB, 1.01% gc time, 6.58% compilation time)
+# 	59.600786 seconds (28.04 M allocations: 1.223 GiB, 1.09% gc time, 6.63% compilation time)
+# 	60.591393 seconds (28.04 M allocations: 1.223 GiB, 1.08% gc time, 6.56% compilation time)
+
+#######
+
+# repeats = 5, d in 1:4, g in 1:3, twisted flag class_one().
+
+# With VPs:
+# 	399.041496 seconds (156.94 M allocations: 6.818 GiB, 0.92% gc time, 1.54% compilation time)
+
+# With H:
+# 	377.038860 seconds (157.99 M allocations: 6.866 GiB, 0.96% gc time, 1.79% compilation time)
